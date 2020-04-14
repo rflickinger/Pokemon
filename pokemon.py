57aa67e365adff0836467dff08dcdc2120658313
rflickinger <rflickinger15@gmail.com>
2020-04-14 14:29:37 -0400

    Added lose health method

--- pokemon.py
+++ pokemon.py
@@ -6,5 +6,25 @@ class Pokemon:
         self.max_health = max_health         #int maximum health
         self.current_health = current_health #int current health
         self.ko = ko                         #bool is knocked out
+    def __repr__(self):                      #set object name to pokemon name
+        return self.name
+    
+    #Method to take damage
+    def lose_health(self, dmg):
+        if dmg > self.current_health:
+            self.current_health = 0
+            self.ko = True                   #Possibly change this to a method
+            print(f"Massive hit! {self.name} took {self.current_health} damage!")
+            print(f"{self.name} has fainted.")#If ko is changed to a method remove this
+        elif dmg == self.current_health:
+            self.current_health = 0
+            self.ko = True
+            print(f"{self.name} took {dmg} damage.")
+            print(f"{self.name} has fainted.")
+        else:
+            self.current_health = self.current_health - dmg
+            print(f"{self.name} took {dmg} damage.")
+    
+
 
 
